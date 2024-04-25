2d599af24956639120002b62e1929fbf5aed4fbd
Rehmet <rehmetjan.tursun@gmail.com>
2024-04-25 15:44:25 +0800

    Fix footer to show current year in main.js

--- main.js
+++ main.js
@@ -1,13 +1,1 @@
-// i want to show current year in the footer
 document.querySelector('footer p').innerHTML += '&copy; ' + new Date().getFullYear() + ' Rehmetjan Tursun. All rights reserved.';
-
-var twoSum = function(nums, target) {
-    let map = new Map();
-    for (let i = 0; i < nums.length; i++) {
-        let complement = target - nums[i];
-        if (map.has(complement)) {
-            return [map.get(complement), i];
-        }
-        map.set(nums[i], i);
-    }
-};
