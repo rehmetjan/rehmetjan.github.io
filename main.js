50a3ced96173d322879438576cdddd80aabf6887
Rehmetjan Tursun <rehmetjan.tursun@gmail.com>
2024-05-03 04:37:25 +0800

    try push in vs code

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 document.querySelector('footer p').innerHTML += '&copy; ' + new Date().getFullYear() + ' Rehmetjan Tursun. All rights reserved.';
+
+// the leetcode twosum function
+var twoSum = function(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        let diff = target - nums[i];
+        let index = nums.indexOf(diff);
+        if (index !== -1 && index !== i) {
+            return [i, index];
+        }
+    }
+};
+
+// which git does vs-code using
+// a: git --version
