c3869c6921c75a960bb60641aead6d0a1e53c817
Rehmetjan Tursun <rehmetjan.tursun@gmail.com>
2024-05-05 23:52:48 +0800

    chore: Remove unnecessary comments and blank line in main.js

--- main.js
+++ main.js
@@ -10,3 +10,32 @@ var twoSum = function(nums, target) {
         }
     }
 };
+
+// 两个数字相加的函数
+var addTwoNumbers = function(l1, l2) {
+    let dummy = new ListNode();
+    let current = dummy;
+    let carry = 0;
+    while (l1 !== null || l2 !== null) {
+        let sum = 0;
+        if (l1 !== null) {
+            sum += l1.val;
+            l1 = l1.next;
+        }
+        if (l2 !== null) {
+            sum += l2.val;
+            l2 = l2.next;
+        }
+        sum += carry;
+        carry = Math.floor(sum / 10);
+        current.next = new ListNode(sum % 10);
+        current = current.next;
+    }
+    if (carry > 0) {
+        current.next = new ListNode(carry);
+    }
+    return dummy.next;
+};
+
+// Q: 世界上最高的楼在哪个国家？
+// A: 世界上最高的楼是位于阿联酋迪拜的哈利法塔，高828米。
